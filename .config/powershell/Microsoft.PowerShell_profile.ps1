d28db8dd83f2ad1b8fb9b357e7c0b180293624bc
Anjar Febrianto <anjar@users.noreply.github.com>
2022-12-23 21:02:18 +0700

    Update Microsoft.PowerShell_profile.ps1

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,1 @@
-oh-my-posh --init --shell pwsh --config ~/clean-detailed.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config 'https://gist.githubusercontent.com/anjar/fd6b231b0c544f93eb911af35c37278d/raw/de635ac12dea0fae8ae2075d8268bebcdc4c2c8a/clean.omp.json' | Invoke-Expression
